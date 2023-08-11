cf66fc28b9a3836d814b267920da1975ebbf13a5
Muhammad Sarmad Saleem <sarmadsaleem.333@gmail.com>
2023-08-12 01:10:58 +0500

    added create post form

--- src/components/CreatePost.js
+++ src/components/CreatePost.js
@@ -7,20 +7,30 @@ export default function CreatePost() {
                 <div className="modal-dialog">
                     <div className="modal-content">
                         <div className="modal-header">
-                            <h5 className="modal-title" id="exampleModalLabel">Modal title</h5>
+                            <h5 className="modal-title" id="exampleModalLabel">Creating Blog</h5>
                             <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                         </div>
-                        <div className="modal-body">
-                            ...
+                        <div className="input-group mb-3">
+                            <input type="text" className="form-control" placeholder="Blog Title" aria-label="Blog Title" aria-describedby="basic-addon1" />
                         </div>
+                        <div className="input-group mb-3">
+                            <textarea type="text" className="form-control" id='blog-text' placeholder="Blog Text" aria-label="Blog Text" aria-describedby="basic-addon1" />
+                        </div>
+                        <div className="input-group mb-3">
+                            <input type="text" className="form-control" placeholder="Blog Type" aria-label="Blog Type" aria-describedby="basic-addon1" />
+                        </div>
+                        <div className="input-group mb-3">
+                            <i class="fa-regular fa-images nav-item"></i>
+                        </div>
+
                         <div className="modal-footer">
                             <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                            <button type="button" className="btn btn-primary">Save changes</button>
+                            <button type="button" className="btn btn-primary">Create</button>
                         </div>
                     </div>
                 </div>
             </div>
-            <button type="button" className="btn btn-success btn-lg m-md-3 " data-bs-toggle="modal" data-bs-target="#create-post-model">Create Post</button>
+            <button type="button" className="btn btn-success btn-lg m-md-3 " data-bs-toggle="modal" data-bs-target="#create-post-model">Create Blog <i className="fa-solid fa-plus"></i></button>
         </div>
     )
 }
