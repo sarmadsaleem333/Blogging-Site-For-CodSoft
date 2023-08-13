a6ea67fc09d2e09258079919c09a8c1474088e78
Muhammad Sarmad Saleem <sarmadsaleem.333@gmail.com>
2023-08-14 01:06:43 +0500

    added my posts

--- src/components/MyProfile.js
+++ src/components/MyProfile.js
@@ -1,24 +1,66 @@
 import React, { useContext, useEffect } from 'react'
 import userContext from '../context/usercontext/userContext';
+import postContext from '../context/postContext';
 
 export default function MyProfile() {
   const context = useContext(userContext);
   const { fetchuser, userDetails } = context;
+  const context2 = useContext(postContext);
+  const { myPosts, getMyPosts } = context2;
+
+
   useEffect(() => {
+    // Fetch user details
     fetchuser();
-  }, [])
+  }, []);
+
+  useEffect(() => {
+    // Fetch user posts
+    getMyPosts();
+    console.log(myPosts)
+  }, []);
 
   return (
-    <>    <div className='d-flex justify-content-center my-3'>
-      <div>
-        <i className="fa-solid fa-user" style={{ fontSize: '100px' }}></i>
+    <>
+      <div className='d-flex justify-content-center my-3'>
+        <div>
+          <i className="fa-solid fa-user" style={{ fontSize: '100px' }}></i>
+        </div>
+        <div className='mx-3 my-1' >
+          <h1>{userDetails.name}</h1>
+          <h6>{userDetails.email}</h6>
+        </div>
       </div>
-      <div className='mx-3 my-1' >
-        <h1>{userDetails.name}</h1>
-        <h6>{userDetails.email}</h6>
+      <div className="row ">
+        <h1 className='d-flex justify-content-center my-3 fst-italic'>Your Blogs</h1>
+        {
+          myPosts.length > 0 ? myPosts.map((post) => {
+            return (
+              <>
+                <div className="col-md-3">
+                  <div className="card mb-3" style={{ maxWidth: '540px' }}>
+                    <div className="row g-0">
+                      <div className="col-md-4">
+                        <img src={require(`../images/${(post.image)}`)} className="img-fluid rounded-start" alt="..." />
+                      </div>
+                      <div className="col-md-8">
+                        <div className="card-body d-flex flex-column h-100">
+                          <h5 className="card-title">{post.topic}</h5>
+                          <p className="card-text">{post.text.slice(0, 100)}...</p>
+                          <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
+                          <i className="fa-solid fa-comment" style={{ cursor: 'pointer' }} data-bs-toggle="modal" data-bs-target="#add-comment"></i>
+                          <div className="btn-primary btn my-2">Read More</div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </>
+            )
+          })
+            : <h4 className='d-flex justify-content-center my-3 '>You have not posted any Blog</h4>
+        }
       </div>
-    </div>
-      <h1 className='d-flex justify-content-center my-3 fst-italic'>Your Blogs</h1>
     </>
 
   )
--- src/context/postState.js
+++ src/context/postState.js
@@ -1,10 +1,11 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import postContext from './postContext'
 import axios from 'axios';
 
 const PostState = (props) => {
 
     const [posts, setPosts] = useState([]);
+    const [myPosts, setMyPosts] = useState([]);
     const host = "http://localhost:5000";
 
     const uploadPost = async (formData) => {
@@ -25,6 +26,16 @@ const PostState = (props) => {
         })
         setPosts(response.data);
     }
+    const getMyPosts = async () => {
+        let response = await axios.get(`${host}/blogging/posts/getmyposts`, {
+            headers: {
+                "Content-Type": "multipart/form-data",
+                "auth-token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNjRkNGIyNmY1MWE5NjU4NjVmY2Q4MzFiIn0sImlhdCI6MTY5MTg0MTA5OH0.-wTE1TlC6goGSg89xElnQLalm61gorog0f2vJVHbPzI",
+            }
+        })
+     
+        setMyPosts(response.data);
+    }
     const searchPosts = async (query) => {
         const result = await axios.get(`${host}/blogging/search/searchitems/?q=${query}`, {
             headers: {
@@ -32,13 +43,15 @@ const PostState = (props) => {
                 "auth-token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNjRkNGIyNmY1MWE5NjU4NjVmY2Q4MzFiIn0sImlhdCI6MTY5MTg0MTA5OH0.-wTE1TlC6goGSg89xElnQLalm61gorog0f2vJVHbPzI",
             }
         })
-        setPosts(result.data);
-    }
-        return (
-            <postContext.Provider value={{ uploadPost, getPosts, posts,searchPosts }}>
-                {props.children}
-            </postContext.Provider>
-        )
-    }
 
-    export default PostState;
+        setPosts(result.data);
+
+    }
+    return (
+        <postContext.Provider value={{ uploadPost, getPosts, posts, searchPosts, myPosts, getMyPosts }}>
+            {props.children}
+        </postContext.Provider>
+    )
+}
+
+export default PostState;
